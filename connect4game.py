8cc746bca8167070b322d19317695bfb18d5add1
Dominique <domjon24@gmail.com>
2021-01-27 23:08:05 -0600

    created board

---
+++ connect4game.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def create_board():
+    board = np.zeros((6,7))
+    return board
+
+board = create_board()
+game_over = False
+print (board)
+
+# while not game_over:
+        # ask p1 inpue
+        # if
+        # ask p2 input
